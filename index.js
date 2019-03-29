29a31ea6a7d27ec0b250632d9f99cbf5b6b01c8f
soer145a <soren.remboll@gmail.com>
2019-03-29 14:00:42 +0100

    Git made buttons for frontpage

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 "use strict";
 import TweenMax from "gsap/TweenMax";
 import Draggable from "gsap/Draggable";
+
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+  console.log("INIT");
+  document.querySelector("#button1").addEventListener("click", () => {
+    document.querySelector("#button1").classList.add("buttonActive");
+    document.querySelector("#button2").classList.remove("buttonActive");
+  });
+  document.querySelector("#button2").addEventListener("click", () => {
+    document.querySelector("#button2").classList.add("buttonActive");
+    document.querySelector("#button1").classList.remove("buttonActive");
+  });
+}
