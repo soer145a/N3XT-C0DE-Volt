f17930c0fa46d2b1f59c2641e28814b312c2a05b
josh-kea <josh0661@stud.kea.dk>
2019-04-02 11:32:51 +0200

    changed js a bit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Draggable from "gsap/Draggable";
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log("INIT");
+  if(document.querySelector("#button1")){
   document.querySelector("#button1").addEventListener("click", () => {
     document.querySelector("#button1").classList.add("buttonActive");
     document.querySelector("#button2").classList.remove("buttonActive");
@@ -13,4 +14,5 @@ function init() {
     document.querySelector("#button2").classList.add("buttonActive");
     document.querySelector("#button1").classList.remove("buttonActive");
   });
+}
 }
